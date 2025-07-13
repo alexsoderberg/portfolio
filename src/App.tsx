267c492eec747823d022ea9be6f274b3fea49f31
Alex Söderberg <soderberg.alex@outlook.com>
2025-07-13 11:40:47 +0200

    Feat: Add toggle for language

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import './App.css'
 import { Routes, Route } from 'react-router';
 import HomePage from './ui/page/HomePage';
 import Layout from './ui/components/Layout';
+import { useState } from 'react';
+import { Languages } from './data/Languages';
 
 function App() {
+  const [currentLanguage, setCurrentLanguage] = useState(Languages.SWEDISH)
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
+        <Route path='/' element={<Layout setLanguage={setCurrentLanguage}/>}>
+          <Route index element={<HomePage language={currentLanguage}/>} />
         </Route>
       </Routes>
     </>
